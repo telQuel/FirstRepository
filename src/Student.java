7c3cc07a95b84f0c80b115aa32bd0704ddaabc3f
Ilya <efgvj45@mail.ru>
2022-08-08 23:21:28 +0300

    Test commit master

--- src/Student.java
+++ src/Student.java
@@ -20,6 +20,7 @@ public class Student {
 }
 
 class StudentInfo {
+    //master test
     void printStudent (Student student) {
         System.out.println("Name student: " + student.name + ", pol: " + student.sex + ", vozrast: " + student.age + ", kurs: " + student.avgGrade);
     }
